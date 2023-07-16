db708453b14f6c7cb15e732f20caf0fbb37d2980
Georgi Yunchov <georgiiunchov@gmail.com>
2023-07-16 18:13:51 +0300

    Add new routes

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { UltracleanServicesComponent } from './ultraclean-services/ultraclean-services.component';
+import { JoinUsComponent } from './join-us/join-us.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 const routes: Routes = [
   {
@@ -15,8 +20,24 @@ const routes: Routes = [
   },
   {
     path: 'about',
-    component: AboutComponent
-  }
+    component: AboutComponent,
+  },
+  {
+    path: 'services',
+    component: UltracleanServicesComponent,
+  },
+  {
+    path: 'application',
+    component: JoinUsComponent,
+  },
+  {
+    path: 'booking-form',
+    component: BookingFormComponent,
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+  },
 ];
 
 @NgModule({
