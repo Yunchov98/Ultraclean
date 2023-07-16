0c5f0b43b30737d30717bd392238ac0ee18e9571
Georgi Yunchov <georgiiunchov@gmail.com>
2023-07-16 18:11:51 +0300

    Add and export components from shared module

--- App/src/app/shared/shared.module.ts
+++ App/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinerComponent } from './spiner/spiner.component';
+import { ContactsButtonComponent } from './contacts-button/contacts-button.component';
+import { SharedRoutingModule } from './shared-routing.module';
+import { CancelButtonComponent } from './cancel-button/cancel-button.component';
 
 @NgModule({
-  declarations: [SpinerComponent],
-  imports: [CommonModule],
-  exports: [SpinerComponent],
+  declarations: [SpinerComponent, ContactsButtonComponent, CancelButtonComponent],
+  imports: [CommonModule, SharedRoutingModule],
+  exports: [SpinerComponent, ContactsButtonComponent, CancelButtonComponent],
 })
 export class SharedModule {}
