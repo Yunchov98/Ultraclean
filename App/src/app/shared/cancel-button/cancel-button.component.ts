6b5c16834a2c199591013450ecd130f09ae19e57
Georgi Yunchov <georgiiunchov@gmail.com>
2023-07-16 18:20:41 +0300

    Add cancel button functionality

--- App/src/app/shared/cancel-button/cancel-button.component.ts
+++ App/src/app/shared/cancel-button/cancel-button.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cancel-button',
   templateUrl: './cancel-button.component.html',
-  styleUrls: ['./cancel-button.component.css']
+  styleUrls: ['./cancel-button.component.css'],
 })
 export class CancelButtonComponent {
+  constructor(private router: Router) {}
 
+  close() {
+    this.router.navigate(['/']);
+  }
 }
