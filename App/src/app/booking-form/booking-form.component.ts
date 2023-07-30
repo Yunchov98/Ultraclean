dd04be007cfcc97f827d8a8a51f6eb3392b03324
Georgi Yunchov <georgiiunchov@gmail.com>
2023-07-30 22:26:23 +0300

    Update

--- App/src/app/booking-form/booking-form.component.ts
+++ App/src/app/booking-form/booking-form.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
-  styleUrls: ['./booking-form.component.css']
+  styleUrls: ['./booking-form.component.css'],
 })
-export class BookingFormComponent {
-
-}
+export class BookingFormComponent {}
