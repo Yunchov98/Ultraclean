1c7ca8f25419972e465eadc10d9f450e65dc3ae4
Georgi Yunchov <georgiiunchov@gmail.com>
2023-07-30 15:04:51 +0300

    Add join-us component

--- App/src/app/join-us/join-us.component.ts
+++ App/src/app/join-us/join-us.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-join-us',
   templateUrl: './join-us.component.html',
-  styleUrls: ['./join-us.component.css']
+  styleUrls: ['./join-us.component.css'],
 })
 export class JoinUsComponent {
+  submitHandler(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
 
+    console.log('clicked');
+  }
 }
