b24b896d26831f3d0ccf5e7a3ddfee0f6fabda24
Georgi Yunchov <georgiiunchov@gmail.com>
2023-07-30 22:25:50 +0300

    Get data dynamically

--- App/src/app/add-service/add-service.component.ts
+++ App/src/app/add-service/add-service.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Database, ref, set } from '@angular/fire/database';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-service',
@@ -7,12 +9,22 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./add-service.component.css'],
 })
 export class AddServiceComponent {
+  constructor(private database: Database, private router: Router) {}
+
   addServiceHandler(form: NgForm): void {
     if (form.invalid) {
       return;
     }
 
-    console.log('clicked');
+    const { service, imageUrl, price, description } = form.value;
+
+    set(ref(this.database, 'booking-services/' + service), {
+      service,
+      imageUrl,
+      price: Number(price),
+      description,
+    });
+
+    this.router.navigate(['/succesfully']);
   }
-  
 }
