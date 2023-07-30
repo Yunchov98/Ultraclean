f13fe4769c286b6dc67a3582c1746d9e1cc9f0a3
Georgi Yunchov <georgiiunchov@gmail.com>
2023-07-30 22:28:07 +0300

    Add interface for job requests

---
+++ App/src/app/interfaces/job-request.ts
@@ -0,0 +1,10 @@
+export interface JobRequest {
+  comments: string;
+  email: string;
+  firstName: string;
+  lastCompany: string;
+  lastName: string;
+  portfolio: string;
+  salary: number;
+  startDate: string;
+}
