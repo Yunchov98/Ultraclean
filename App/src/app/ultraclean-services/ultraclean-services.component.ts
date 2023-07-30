1853aedf745a307dc15c4b3c4ce73cf88f6d8386
Georgi Yunchov <georgiiunchov@gmail.com>
2023-07-30 22:32:17 +0300

    Get data dynamically

--- App/src/app/ultraclean-services/ultraclean-services.component.ts
+++ App/src/app/ultraclean-services/ultraclean-services.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Database, onValue, ref } from '@angular/fire/database';
+
+import { Service } from '../interfaces/service';
 
 @Component({
   selector: 'app-ultraclean-services',
   templateUrl: './ultraclean-services.component.html',
-  styleUrls: ['./ultraclean-services.component.css']
+  styleUrls: ['./ultraclean-services.component.css'],
 })
-export class UltracleanServicesComponent {
+export class UltracleanServicesComponent implements OnInit {
+  constructor(private database: Database) {}
+  services: Service[] = [];
+  isClicked = false;
+  isLoading = true;
 
+  ngOnInit(): void {
+    const starCountRef = ref(this.database, 'booking-services');
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+
+      Object.keys(data).forEach((key) => {
+        this.services.push(data[key]);
+      });
+
+      this.isLoading = false;
+      console.log(this.services);
+    });
+  }
+
+  toggle(): void {
+    this.isClicked = !this.isClicked;
+  }
 }
