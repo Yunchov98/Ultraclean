6243c85dfe0b832fcb5cfae0e9fc4f0637683e45
Georgi Yunchov <georgiiunchov@gmail.com>
2023-07-30 22:33:33 +0300

    Add environments

--- App/src/environments/environment.development.ts
+++ App/src/environments/environment.development.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'ultraclean-1f04b',
+    appId: '1:802926662215:web:93029d56283c3a6ddb1762',
+    databaseURL: 'https://ultraclean-1f04b-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'ultraclean-1f04b.appspot.com',
+    apiKey: 'AIzaSyBhFKkdK-kfWpGSFSZ3H79mEU2orSXPWj0',
+    authDomain: 'ultraclean-1f04b.firebaseapp.com',
+    messagingSenderId: '802926662215',
+  },};
