9a30da08b466128282726da0cdf0e3dff815c5a2
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-01 23:51:54 +0300

    Add booking-form dynamically data

--- App/src/app/booking-form/booking-form.component.ts
+++ App/src/app/booking-form/booking-form.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ref, onValue, Database } from '@angular/fire/database';
+import { Service } from '../interfaces/service';
 
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.css'],
 })
-export class BookingFormComponent {}
+export class BookingFormComponent implements OnInit {
+  constructor(private database: Database) {}
+
+  services: Service[] = [];
+  isLoading: boolean = true;
+  selectedServices: string[] = [];
+
+  ngOnInit(): void {
+    const starCountRef = ref(this.database, 'booking-services');
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+
+      Object.keys(data).forEach((key) => {
+        this.services.push(data[key]);
+      });
+
+      this.isLoading = false;
+    });
+  }
+}
