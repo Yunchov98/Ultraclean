1c6aeced93e49706611ca30ae9d68fabf4adf90d
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 21:00:28 +0300

    Add auth service

---
+++ App/src/app/app-services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/User';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  getUserData(): User | null {
+    const token = localStorage.getItem('userData');
+    if (!token) {
+      return null;
+    }
+
+    return JSON.parse(token);
+  }
+
+  setUserData(data: User): void {
+    localStorage.setItem('userData', JSON.stringify(data));
+  }
+
+  clearUserData(): void {
+    localStorage.removeItem('userData');
+  }
+
+  get isLoggedIn(): boolean {
+    return localStorage.getItem('userData') ? true : false;
+  }
+}
