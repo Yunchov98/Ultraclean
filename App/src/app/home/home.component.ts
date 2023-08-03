248e73ccff0940238d995662a10c67ae799d5f28
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 22:10:14 +0300

    Add dynamic home view

--- App/src/app/home/home.component.ts
+++ App/src/app/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../app-services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor(private authService: AuthService) {}
+  
+  isAdmin(): boolean {
+    if (
+      this.authService.getUserData()?.email?.toLowerCase().includes('admin')
+    ) {
+      return true;
+    }
 
+    return false;
+  }
 }
