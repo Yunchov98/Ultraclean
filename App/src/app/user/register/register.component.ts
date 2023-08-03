5c9024fc2113517a4f39764db29efc521f79f2be
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 22:12:37 +0300

    Add register functionality

--- App/src/app/user/register/register.component.ts
+++ App/src/app/user/register/register.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/app-services/auth.service';
+import { UserService } from 'src/app/app-services/user.service';
+import { User } from 'src/app/interfaces/User';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  errorMessage!: string;
+  userData!: User;
 
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  registerHandler(registerForm: NgForm) {
+    if (registerForm.invalid) {
+      return;
+    }
+
+    const { email, password } = registerForm.value;
+
+    this.userService
+      .register(email, password)
+      .then(() =>
+        this.userService.login(email, password).then((data) => {
+          this.userData = { _id: data.user?.uid, email: data.user?.email };
+
+          this.authService.setUserData(this.userData);
+
+          this.router.navigate(['/']);
+        })
+      )
+      .catch((err) => (this.errorMessage = 'Email already exists!'))
+      .catch((err) => (this.errorMessage = 'Email already exists!'));
+  }
 }
