521027662e4d278f678d8f313afe57c3dcff685c
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 22:09:51 +0300

    Add dynamic user header

--- App/src/app/core/header/user-header/user-header.component.ts
+++ App/src/app/core/header/user-header/user-header.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/app-services/auth.service';
 
 @Component({
   selector: 'app-user-header',
   templateUrl: './user-header.component.html',
-  styleUrls: ['./user-header.component.css']
+  styleUrls: ['./user-header.component.css'],
 })
 export class UserHeaderComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  logoutHandler() {
+    this.authService.clearUserData();
+
+    this.router.navigate(['/']);
+  }
 }
