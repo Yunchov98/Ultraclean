59a8840506a1ca40c675c913c218cff460360ef9
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 22:07:18 +0300

    Add logout component

---
+++ App/src/app/shared/logout/logout.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/app-services/auth.service';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css'],
+})
+export class LogoutComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  logoutHandler() {
+    this.authService.clearUserData();
+
+    this.router.navigate(['/']);
+  }
+}
--- App/src/app/shared/shared.module.ts
+++ App/src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { ContactsButtonComponent } from './contacts-button/contacts-button.compo
 import { SharedRoutingModule } from './shared-routing.module';
 import { CancelButtonComponent } from './cancel-button/cancel-button.component';
 import { SuccessfullyComponent } from './successfully/successfully.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
-  declarations: [SpinerComponent, ContactsButtonComponent, CancelButtonComponent, SuccessfullyComponent],
+  declarations: [SpinerComponent, ContactsButtonComponent, CancelButtonComponent, SuccessfullyComponent, LogoutComponent],
   imports: [CommonModule, SharedRoutingModule],
-  exports: [SpinerComponent, ContactsButtonComponent, CancelButtonComponent, SuccessfullyComponent],
+  exports: [SpinerComponent, ContactsButtonComponent, CancelButtonComponent, SuccessfullyComponent, LogoutComponent],
 })
 export class SharedModule {}
