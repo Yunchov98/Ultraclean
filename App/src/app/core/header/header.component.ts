50be7f15346ea3c2d2ec6aab344cfaf150407a2e
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 22:16:20 +0300

    Add dynamically header

--- App/src/app/core/header/header.component.ts
+++ App/src/app/core/header/header.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/app-services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService) {}
 
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  isAdmin(): boolean {
+    if (
+      this.authService.getUserData()?.email?.toLowerCase().includes('admin')
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  isUser(): boolean {
+    if (
+      !this.authService.getUserData()?.email?.toLowerCase().includes('admin')
+    ) {
+      return true;
+    }
+
+    return false;
+  }
 }
