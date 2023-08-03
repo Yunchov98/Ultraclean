cb884b73402abd79e28fa41ea3fdceba826d57ae
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 22:13:05 +0300

    Add firebase options to user module

--- App/src/app/user/user.module.ts
+++ App/src/app/user/user.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 import { LoginComponent } from './login/login.component';
 import { UserRoutingModule } from './user-routing.module';
 import { RegisterComponent } from './register/register.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   imports: [CommonModule, UserRoutingModule, FormsModule],
   exports: [LoginComponent, RegisterComponent],
+  providers: [{ provide: FIREBASE_OPTIONS, useValue: environment.firebase }],
 })
 export class UserModule {}
