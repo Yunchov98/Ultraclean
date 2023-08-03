98a94563137e22d8d4cc713fcdbf3ccc217fd321
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 21:03:12 +0300

    Add user service

---
+++ App/src/app/app-services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private fireAuth: AngularFireAuth) {}
+
+  login(email: string, password: string) {
+    return this.fireAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  register(email: string, password: string) {
+    return this.fireAuth.createUserWithEmailAndPassword(email, password);
+  }
+}
