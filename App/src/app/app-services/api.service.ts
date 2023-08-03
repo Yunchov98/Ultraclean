7756fe0c9b5ad9be6c3d7503de258ed264daa9e0
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 21:00:02 +0300

    Add api service

---
+++ App/src/app/app-services/api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+import { Service } from '../interfaces/Service';
+import { JobRequest } from '../interfaces/Job-request';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  getBookingServices(): Observable<Service[]> {
+    const { firebase } = environment;
+    return this.http.get<Service[]>(
+      `${firebase.databaseURL}/booking-services.json`
+    );
+  }
+
+  getJobRequests(): Observable<JobRequest[]> {
+    const { firebase } = environment;
+    return this.http.get<JobRequest[]>(
+      `${firebase.databaseURL}/job-requests.json`
+    );
+  }
+}
