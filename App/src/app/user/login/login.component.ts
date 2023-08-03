782e3210e6faa73482c3093702284e64e87842a3
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 22:12:02 +0300

    Add login functionality

--- App/src/app/user/login/login.component.ts
+++ App/src/app/user/login/login.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { User } from '../../interfaces/User';
+import { AuthService } from 'src/app/app-services/auth.service';
+import { UserService } from 'src/app/app-services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  errorMessage!: string;
 
+  userData!: User;
+
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  loginHandler(loginForm: NgForm): void {
+    const { email, password } = loginForm.value;
+
+    this.userService
+      .login(email, password)
+      .then((data) => {
+        this.userData = { _id: data.user?.uid, email: data.user?.email };
+
+        this.authService.setUserData(this.userData);
+
+        this.router.navigate(['/']);
+      })
+      .catch((err) => (this.errorMessage = 'Wrong email or password!'));
+  }
 }
