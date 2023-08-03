6b6351a11299ed103c306777e96463237d6b1af1
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 22:08:51 +0300

    Add logout component wwith logout functionality

--- App/src/app/core/header/admin-header/admin-header.component.ts
+++ App/src/app/core/header/admin-header/admin-header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/app-services/auth.service';
 
 @Component({
   selector: 'app-admin-header',
   templateUrl: './admin-header.component.html',
-  styleUrls: ['./admin-header.component.css']
+  styleUrls: ['./admin-header.component.css'],
 })
 export class AdminHeaderComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  logoutHandler(): void {
+    this.authService.clearUserData();
+
+    this.router.navigate(['/']);
+  }
 }
