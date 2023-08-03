09e4593842784e83c9618a3f31258a23c2f65217
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-03 22:14:15 +0300

    Add dynamicaly view on uc-services

--- App/src/app/ultraclean-services/ultraclean-services.component.ts
+++ App/src/app/ultraclean-services/ultraclean-services.component.ts
@@ -1,34 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { Database, onValue, ref } from '@angular/fire/database';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, map } from 'rxjs';
 
-import { Service } from '../interfaces/service';
+import { Service } from '../interfaces/Service';
+import { ApiService } from '../app-services/api.service';
+import { AuthService } from '../app-services/auth.service';
 
 @Component({
   selector: 'app-ultraclean-services',
   templateUrl: './ultraclean-services.component.html',
   styleUrls: ['./ultraclean-services.component.css'],
 })
-export class UltracleanServicesComponent implements OnInit {
-  constructor(private database: Database) {}
+export class UltracleanServicesComponent implements OnInit, OnDestroy {
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService
+  ) {}
+
   services: Service[] = [];
-  isClicked = false;
-  isLoading = true;
+  subscribe$!: Subscription;
+  isClicked: boolean = false;
+  isLoading: boolean = true;
+
+  isAdmin(): boolean {
+    if (
+      this.authService.getUserData()?.email?.toLowerCase().includes('admin')
+    ) {
+      return true;
+    }
+
+    return false;
+  }
 
   ngOnInit(): void {
-    const starCountRef = ref(this.database, 'booking-services');
-    onValue(starCountRef, (snapshot) => {
-      const data = snapshot.val();
+    this.subscribe$ = this.apiService
+      .getBookingServices()
+      .pipe(
+        map((serv) => {
+          const services: Service[] = [];
+          for (const key in serv) {
+            if (serv.hasOwnProperty(key)) {
+              services.push({ ...serv[key], _id: key });
+            }
+          }
 
-      Object.keys(data).forEach((key) => {
-        this.services.push(data[key]);
+          return services;
+        })
+      )
+      .subscribe({
+        next: (services: Service[]) => {
+          this.services = services;
+          this.isLoading = false;
+        },
+        error: (error) => console.log(`Error: ${error}`),
       });
-
-      this.isLoading = false;
-      console.log(this.services);
-    });
   }
 
   toggle(): void {
     this.isClicked = !this.isClicked;
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscribe$ !== undefined) {
+      console.log('unsub');
+      this.subscribe$.unsubscribe();
+    }
   }
 }
