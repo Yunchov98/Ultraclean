56beffcc2efcca1ee9ad10a0821d8c5c910de710
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-05 17:19:12 +0300

    Add cleaning request functionality

--- App/src/app/cleaning-requests/cleaning-requests.component.ts
+++ App/src/app/cleaning-requests/cleaning-requests.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, map } from 'rxjs';
+
+import { ApiService } from '../app-services/api.service';
+import { CleaningRequest } from '../interfaces/Cleaning-request';
 
 @Component({
   selector: 'app-cleaning-requests',
   templateUrl: './cleaning-requests.component.html',
-  styleUrls: ['./cleaning-requests.component.css']
+  styleUrls: ['./cleaning-requests.component.css'],
 })
-export class CleaningRequestsComponent {
+export class CleaningRequestsComponent implements OnInit, OnDestroy {
+  subscription$!: Subscription;
+  cleaningRequests: CleaningRequest[] = [];
+  isLoading = true;
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.subscription$ = this.apiService
+      .getCleaningRequests()
+      .pipe(
+        map((reqs) => {
+          const requests: CleaningRequest[] = [];
+          for (const key in reqs) {
+            if (reqs.hasOwnProperty(key)) {
+              requests.push({ ...reqs[key], _id: key });
+            }
+          }
+
+          return requests;
+        })
+      )
+      .subscribe({
+        next: (requests: CleaningRequest[]) => {
+          this.cleaningRequests = requests;
+          this.isLoading = false;
+        },
+        error: (error) => console.log(`Error: ${error}`),
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription$ !== undefined) {
+      this.subscription$.unsubscribe();
+    }
+  }
 }
