a58ff3c604a00c15fb1f075277e1c6af58cfd163
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-05 17:01:38 +0300

    Add booking-form functionality

--- App/src/app/booking-form/booking-form.component.ts
+++ App/src/app/booking-form/booking-form.component.ts
@@ -1,29 +1,105 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ref, onValue, Database } from '@angular/fire/database';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription, map } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
+
 import { Service } from '../interfaces/Service';
+import { ApiService } from '../app-services/api.service';
+import { CleaningRequest } from '../interfaces/Cleaning-request';
+import { AuthService } from '../app-services/auth.service';
 
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.css'],
 })
-export class BookingFormComponent implements OnInit {
-  constructor(private database: Database) {}
-
+export class BookingFormComponent implements OnInit, OnDestroy {
+  subscription$!: Subscription;
   services: Service[] = [];
+  selectedServices: Service[] = [];
+  request!: CleaningRequest;
   isLoading: boolean = true;
-  selectedServices: string[] = [];
+  errorMessage: string = '';
+  totalPrice: number = 0;
+
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    const starCountRef = ref(this.database, 'booking-services');
-    onValue(starCountRef, (snapshot) => {
-      const data = snapshot.val();
+    this.subscription$ = this.apiService
+      .getUltracleanServices()
+      .pipe(
+        map((serv) => {
+          const services: Service[] = [];
 
-      Object.keys(data).forEach((key) => {
-        this.services.push(data[key]);
+          for (const key in serv) {
+            if (serv.hasOwnProperty(key)) {
+              services.push({ ...serv[key], _id: key });
+            }
+          }
+
+          return services;
+        })
+      )
+      .subscribe({
+        next: (services: Service[]) => {
+          this.services = services;
+
+          this.isLoading = false;
+        },
+        error: (error) => console.log(`Error: ${error}`),
       });
+  }
 
-      this.isLoading = false;
+  createRequest(bookingForm: NgForm) {
+    if (bookingForm.invalid) {
+      this.errorMessage = 'Fields with * are required!';
+
+      throw new Error('Fields with * are required!');
+    }
+
+    if (this.selectedServices.length < 1) {
+      throw new Error('Select a service(s)');
+    }
+
+    const userId = this.authService.getUserData()?._id;
+
+    const { name, email, phone, address, city } = bookingForm.value;
+
+    this.request = {
+      _id: uuidv4(),
+      _ownerId: userId,
+      name,
+      email,
+      phone,
+      address,
+      city,
+      services: this.selectedServices,
+      createdAt: new Date().toString(),
+      totalPrice: this.totalPrice,
+    };
+
+    this.subscription$ = this.apiService.bookAService(this.request).subscribe({
+      next: () => this.router.navigate(['/successfully']),
+      error: (error) => console.log(error),
     });
+  }
+
+  sumTotalPrice(): number {
+    this.totalPrice = this.selectedServices.reduce((total, curService) => {
+      return total + curService.price;
+    }, 0);
+
+    return 0;
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription$ !== undefined) {
+      this.subscription$.unsubscribe();
+    }
   }
 }
