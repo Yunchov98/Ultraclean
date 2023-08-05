dd4b5361c11a1c6bbfcdf4a8c24f68cdabd8f98f
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-05 18:07:22 +0300

    Add loading spiner

--- App/src/app/contacts/contacts.component.ts
+++ App/src/app/contacts/contacts.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
+  isLoading: boolean = true;
 
+  ngOnInit(): void {
+    this.isLoading = false;
+  }
 }
