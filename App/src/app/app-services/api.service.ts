6db2575dcc8f966278b6829d0563e21aefb7bfb8
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-06 21:58:11 +0300

    Update api-service

--- App/src/app/app-services/api.service.ts
+++ App/src/app/app-services/api.service.ts
@@ -6,49 +6,55 @@ import { environment } from '../../environments/environment';
 import { Service } from '../interfaces/Service';
 import { JobRequest } from '../interfaces/Job-request';
 import { CleaningRequest } from '../interfaces/Cleaning-request';
+import { User } from '../interfaces/User';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  apiUrl: string = environment.firebase.databaseURL;
+
   constructor(private http: HttpClient) {}
 
   getUltracleanServices(): Observable<Service[]> {
-    const { firebase } = environment;
-    return this.http.get<Service[]>(
-      `${firebase.databaseURL}/ultraclean-services.json`
+    return this.http.get<Service[]>(`${this.apiUrl}/ultraclean-services.json`);
+  }
+
+  getServiceById(id: string): Observable<Service> {
+    return this.http.get<Service>(
+      `${this.apiUrl}/ultraclean-services/${id}.json`
     );
   }
 
   getCleaningRequests(): Observable<CleaningRequest[]> {
-    const { firebase } = environment;
     return this.http.get<CleaningRequest[]>(
-      `${firebase.databaseURL}/cleaning-requests.json`
+      `${this.apiUrl}/cleaning-requests.json`
     );
   }
 
   getJobRequests(): Observable<JobRequest[]> {
-    const { firebase } = environment;
-    return this.http.get<JobRequest[]>(
-      `${firebase.databaseURL}/job-requests.json`
-    );
+    return this.http.get<JobRequest[]>(`${this.apiUrl}/job-requests.json`);
   }
 
   createService(data: Service): Observable<Service> {
-    const { firebase } = environment;
-
     return this.http.post<Service>(
-      `${firebase.databaseURL}/ultraclean-services.json`,
+      `${this.apiUrl}/ultraclean-services.json`,
       data
     );
   }
 
   bookAService(data: CleaningRequest): Observable<CleaningRequest> {
-    const { firebase } = environment;
-
     return this.http.post<CleaningRequest>(
-      `${firebase.databaseURL}/cleaning-requests.json`,
+      `${this.apiUrl}/cleaning-requests.json`,
       data
     );
+  }
+
+  sendJobRequest(data: JobRequest): Observable<JobRequest> {
+    return this.http.post<JobRequest>(`${this.apiUrl}/job-requests.json`, data);
+  }
+
+  addUser(userData: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/users.json`, userData);
   }
 }
