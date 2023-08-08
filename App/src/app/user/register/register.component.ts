2757c9f1b4f23e24927e3aa303ecc6f1966a19ca
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-08 21:58:52 +0300

    Update register ts component

--- App/src/app/user/register/register.component.ts
+++ App/src/app/user/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/app-services/api.service';
 import { AuthService } from 'src/app/app-services/auth.service';
 import { UserService } from 'src/app/app-services/user.service';
 import { User } from 'src/app/interfaces/User';
@@ -14,11 +15,14 @@ export class RegisterComponent {
   errorMessage: string = '';
   userData!: User;
   emailErrorMessage: string = '';
+  token: string = '';
+  user!: User;
 
   constructor(
     private userService: UserService,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private apiService: ApiService
   ) {}
 
   registerHandler(registerForm: NgForm) {
@@ -35,18 +39,25 @@ export class RegisterComponent {
 
     this.userService
       .register(email, password)
-      .then(() =>
-        this.userService
-          .login(email, password)
-          .then((data) => {
-            this.userData = { _id: data.user?.uid, email: data.user?.email };
+      .then((data) => {
+        data.user
+          ?.getIdToken()
+          .then((t) => {
+            this.userData = {
+              _id: data.user?.uid,
+              email: data.user?.email,
+              token: t,
+            };
 
             this.authService.setUserData(this.userData);
 
-            this.router.navigate(['/']);
+            this.apiService.addUser(this.userData).subscribe({
+              next: () => this.router.navigate(['/successfully']),
+              error: (err) => console.log(err),
+            });
           })
-          .catch((err) => console.log(err))
-      )
-      .catch(() => (this.errorMessage = 'Email already exists!'));
+          .catch((err) => console.log(err));
+      })
+      .catch(() => (this.errorMessage = 'This email already exist!'));
   }
 }
