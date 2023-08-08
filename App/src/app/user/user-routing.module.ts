e6949d06f6374280cdc9f8bb5d33f3c24929b56d
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-08 21:58:40 +0300

    Add guards

--- App/src/app/user/user-routing.module.ts
+++ App/src/app/user/user-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { isGuesttGuard } from '../core/guards/is-guest.guard';
 
 const routes: Routes = [
   {
     path: 'login',
+    canActivate: [isGuesttGuard],
     component: LoginComponent,
   },
   {
     path: 'register',
+    canActivate: [isGuesttGuard],
     component: RegisterComponent,
   },
 ];
