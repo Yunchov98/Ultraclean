37f303a0e0c2ea8dace87c318d1572c0d3429b96
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-08 21:58:30 +0300

    Update error-page ts and html

--- App/src/app/error-page/error-page.component.ts
+++ App/src/app/error-page/error-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../app-services/auth.service';
 
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.css']
+  styleUrls: ['./error-page.component.css'],
 })
-export class ErrorPageComponent {
+export class ErrorPageComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn;
+  }
 }
