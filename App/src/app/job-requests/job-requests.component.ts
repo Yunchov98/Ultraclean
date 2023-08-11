1d3e0236fb6ba0c466a0383122c1a73e171089b0
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-11 21:10:35 +0300

    Update

--- App/src/app/job-requests/job-requests.component.ts
+++ App/src/app/job-requests/job-requests.component.ts
@@ -4,6 +4,7 @@ import { map } from 'rxjs/operators';
 
 import { JobRequest } from '../interfaces/Job-request';
 import { ApiService } from '../app-services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-job-requests',
@@ -14,10 +15,9 @@ export class JobRequestsComponent implements OnInit, OnDestroy {
   isLoading: boolean = true;
   requests: JobRequest[] = [];
   subscription$!: Subscription;
+  requestSubscription$!: Subscription;
 
-  constructor(
-    private apiService: ApiService,
-  ) {}
+  constructor(private apiService: ApiService, private router: Router) {}
 
   ngOnInit(): void {
     this.subscription$ = this.apiService
@@ -43,9 +43,32 @@ export class JobRequestsComponent implements OnInit, OnDestroy {
       });
   }
 
+  acceptHandle(requestId: string) {
+    this.requestSubscription$ = this.apiService
+      .getJobRequestById(requestId)
+      .subscribe({
+        next: (request) => {
+          this.apiService.addWorker(request).subscribe({
+            next: () => {
+              this.apiService.deleteJobRequest(requestId).subscribe({
+                next: () => this.router.navigate(['/successfully']),
+                error: (err) => console.log(err),
+              });
+            },
+            error: (err) => console.log(err),
+          });
+        },
+        error: (err) => console.log(err),
+      });
+  }
+
   ngOnDestroy(): void {
     if (this.subscription$ !== undefined) {
       this.subscription$.unsubscribe();
+    }
+
+    if (this.requestSubscription$ !== undefined) {
+      this.requestSubscription$.unsubscribe();
     }
   }
 }
