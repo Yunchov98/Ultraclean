73aa3f1d7633558233e87c967ad7c0f453fab899
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-11 21:10:10 +0300

    Update user interface

--- App/src/app/interfaces/User.ts
+++ App/src/app/interfaces/User.ts
@@ -1,5 +1,10 @@
+import { Service } from './Service';
+
 export interface User {
   _id: string | undefined;
   email: string | undefined | null;
   token: string;
+  currentOrder: Service[];
+  myOrders: Service[];
+  isAdmin: boolean;
 }
