697ede59f2972a70558fdaee609404dd358b380d
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-11 21:04:07 +0300

    Update

--- App/src/app/app-services/api.service.ts
+++ App/src/app/app-services/api.service.ts
@@ -7,6 +7,9 @@ import { Service } from '../interfaces/Service';
 import { JobRequest } from '../interfaces/Job-request';
 import { CleaningRequest } from '../interfaces/Cleaning-request';
 import { User } from '../interfaces/User';
+import { AcceptedRequest } from '../interfaces/Accepted-request';
+import { FinishedRequest } from '../interfaces/Finished-request';
+import { RejectedRequest } from '../interfaces/Rejected-request';
 
 @Injectable({
   providedIn: 'root',
@@ -18,6 +21,10 @@ export class ApiService {
 
   getUltracleanServices(): Observable<Service[]> {
     return this.http.get<Service[]>(`${this.apiUrl}/ultraclean-services.json`);
+  }
+
+  deleteUltraCleanService(id: string): Observable<unknown> {
+    return this.http.delete(`${this.apiUrl}/ultraclean-services/${id}.json`);
   }
 
   getServiceById(id: string): Observable<Service> {
@@ -53,15 +60,90 @@ export class ApiService {
     );
   }
 
+  getCleaningRequestById(id: string): Observable<CleaningRequest> {
+    return this.http.get<CleaningRequest>(
+      `${this.apiUrl}/cleaning-requests/${id}.json`
+    );
+  }
+
   getJobRequests(): Observable<JobRequest[]> {
     return this.http.get<JobRequest[]>(`${this.apiUrl}/job-requests.json`);
+  }
+
+  getJobRequestById(id: string): Observable<JobRequest> {
+    return this.http.get<JobRequest>(`${this.apiUrl}/job-requests/${id}.json`);
   }
 
   sendJobRequest(data: JobRequest): Observable<JobRequest> {
     return this.http.post<JobRequest>(`${this.apiUrl}/job-requests.json`, data);
   }
 
-  addUser(userData: User): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}/users.json`, userData);
+  deleteJobRequest(id: string): Observable<unknown> {
+    return this.http.delete(`${this.apiUrl}/job-requests/${id}.json`);
+  }
+
+  acceptRequest(data: AcceptedRequest): Observable<AcceptedRequest> {
+    return this.http.post<AcceptedRequest>(
+      `${this.apiUrl}/accepted-requests.json`,
+      data
+    );
+  }
+
+  getAcceptedRequests(): Observable<AcceptedRequest[]> {
+    return this.http.get<AcceptedRequest[]>(
+      `${this.apiUrl}/accepted-requests.json`
+    );
+  }
+
+  getAcceptedRequestById(id: string): Observable<AcceptedRequest> {
+    return this.http.get<AcceptedRequest>(
+      `${this.apiUrl}/accepted-requests/${id}.json`
+    );
+  }
+
+  deleteAcceptedRequest(id: string): Observable<unknown> {
+    return this.http.delete(`${this.apiUrl}/accepted-requests/${id}.json`);
+  }
+
+  addFinishedRequest(data: FinishedRequest): Observable<FinishedRequest> {
+    return this.http.post<FinishedRequest>(
+      `${this.apiUrl}/finished-requests.json`,
+      data
+    );
+  }
+
+  getFinishedRequests(): Observable<FinishedRequest[]> {
+    return this.http.get<FinishedRequest[]>(
+      `${this.apiUrl}/finished-requests.json`
+    );
+  }
+
+  deleteCleaningRequest(id: string): Observable<unknown> {
+    return this.http.delete(`${this.apiUrl}/cleaning-requests/${id}.json`);
+  }
+
+  addRejectedRequest(data: RejectedRequest): Observable<RejectedRequest> {
+    return this.http.post<RejectedRequest>(
+      `${this.apiUrl}/rejected-requests.json`,
+      data
+    );
+  }
+
+  getRejectedRequests(): Observable<RejectedRequest[]> {
+    return this.http.get<RejectedRequest[]>(
+      `${this.apiUrl}/rejected-requests.json`
+    );
+  }
+
+  addWorker(data: JobRequest): Observable<JobRequest> {
+    return this.http.post<JobRequest>(`${this.apiUrl}/workers.json`, data);
+  }
+
+  getWorkers(): Observable<JobRequest[]> {
+    return this.http.get<JobRequest[]>(`${this.apiUrl}/workers.json`);
+  }
+
+  deleteWorker(id: string): Observable<unknown> {
+    return this.http.delete(`${this.apiUrl}/workers/${id}.json`);
   }
 }
