0196858993fd10e4353c78afe80849b9aa7b0b49
Georgi Yunchov <georgiiunchov@gmail.com>
2023-08-11 21:04:26 +0300

    Add "add-user" functionality

--- App/src/app/app-services/user.service.ts
+++ App/src/app/app-services/user.service.ts
@@ -1,11 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
+
+import { User } from '../interfaces/User';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private fireAuth: AngularFireAuth) {}
+  apiUrl: string = environment.firebase.databaseURL;
+
+  constructor(private fireAuth: AngularFireAuth, private http: HttpClient) {}
 
   login(email: string, password: string) {
     return this.fireAuth.signInWithEmailAndPassword(email, password);
@@ -13,5 +20,17 @@ export class UserService {
 
   register(email: string, password: string) {
     return this.fireAuth.createUserWithEmailAndPassword(email, password);
+  }
+
+  addUser(userData: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/users.json`, userData);
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users.json`);
+  }
+
+  getUserById(id: string): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/${id}.json`);
   }
 }
